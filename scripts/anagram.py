4b2bd82cc7633986f4b600aa7602fd4d55185373
Angelica <Angelica137@users.noreply.github.com>
2021-06-27 21:31:22 +0100

    return correct anagrams for a longer list, green

--- scripts/anagram.py
+++ scripts/anagram.py
@@ -1,5 +1,6 @@
 class Anagram:
     def findAnagram(self, str, arr):
+        anagrams = []
         for item in range(len(arr)):
             count = 0
             for char in arr[item]:
@@ -9,11 +10,15 @@ class Anagram:
                         break
             if count == len(arr[item]):
                 print("is anagram")
-                return arr
+                anagrams.append(arr[item])
+                print(anagrams)
             else:
                 print("no anagram")
+        return anagrams
 
 
 a = Anagram()
-
 a.findAnagram("list", ["lit"])
+
+a2 = Anagram()
+a2.findAnagram("list", ["lit", "it", "otter"])
