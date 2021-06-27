102553e3f6c86a80a3bb76d2ef0663ce6449d45e
Angelica <Angelica137@users.noreply.github.com>
2021-06-27 21:24:05 +0100

    return correct anagrams for longer list, red

--- scripts/anagram.py
+++ scripts/anagram.py
@@ -1,19 +1,19 @@
 class Anagram:
     def findAnagram(self, str, arr):
-        count = 0
-        for i in range(len(arr)):
-            print(arr[i])
-            for j in range(len(str)):
-                if arr[i] == str[j]:
-                    count += 1
-                    break
-        if count == len(arr):
-            print("is anagram")
-            return arr
-        else:
-            print("no anagram")
+        for item in range(len(arr)):
+            count = 0
+            for char in arr[item]:
+                for j in range(len(str)):
+                    if char == str[j]:
+                        count += 1
+                        break
+            if count == len(arr[item]):
+                print("is anagram")
+                return arr
+            else:
+                print("no anagram")
 
 
 a = Anagram()
 
-a.findAnagram("list", "lit")
+a.findAnagram("list", ["lit"])
--- tests/test_anagram.py
+++ tests/test_anagram.py
@@ -3,4 +3,9 @@ from scripts.anagram import Anagram
 
 def test_anagram_returns_anagram_if_anagram():
     a = Anagram()
-    assert a.findAnagram("list", "lit") == "lit"
+    assert a.findAnagram("list", ["lit"]) == ["lit"]
+
+
+def test_anargam_returns_correct_anagrams_when_list_greater_one():
+    a = Anagram()
+    assert a.findAnagram("list", ["lit", "it", "otter"]) == ["lit", "it"]
