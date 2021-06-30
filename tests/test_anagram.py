5795ab52bece45683f24e67551091f9ae4791a80
Angelica <Angelica137@users.noreply.github.com>
2021-06-30 21:11:41 +0100

    update tests for anagram rules

--- tests/test_anagram.py
+++ tests/test_anagram.py
@@ -1,11 +1,16 @@
 from scripts.anagram import Anagram
 
 
+def test_findAnagram_knows_word_is_same_length():
+    a = Anagram()
+    assert len(a.findAnagram("list", ["istl"][0])) == 4
+
+
 def test_anagram_returns_anagram_if_anagram():
     a = Anagram()
-    assert a.findAnagram("list", ["lit"]) == ["lit"]
+    assert a.findAnagram("list", ["lit"]) == []
 
 
 def test_anargam_returns_correct_anagrams_when_list_greater_one():
     a = Anagram()
-    assert a.findAnagram("list", ["lit", "it", "otter"]) == ["lit", "it"]
+    assert a.findAnagram("list", ["lit", "it", "otter"]) == []
