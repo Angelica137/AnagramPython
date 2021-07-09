465cd6b0a94c08115de3a03d3a239edf45e647a7
Angelica <Angelica137@users.noreply.github.com>
2021-07-09 22:36:30 +0100

    remove class, second test pass

--- tests/test_anagram.py
+++ tests/test_anagram.py
@@ -5,9 +5,8 @@ def test_findAnagram_knows_word_is_same_length():
     assert len(findAnagram("list", ["istl"])[0]) == 4
 
 
-# def test_anagram_returns_anagram_if_anagram():
-#     a=Anagram()
-#     assert a.findAnagram("list", ["istl"]) == ["istl"]
+def test_anagram_returns_anagram_if_anagram():
+    assert findAnagram("list", ["istl"]) == ["istl"]
 #
 #
 # def test_anargam_returns_correct_anagrams_when_list_greater_one():
