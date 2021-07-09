4bafcac2f60b0652320561088d21577e58bf233d
Angelica <Angelica137@users.noreply.github.com>
2021-07-09 22:37:50 +0100

    remove class, all tests pass

--- tests/test_anagram.py
+++ tests/test_anagram.py
@@ -7,14 +7,12 @@ def test_findAnagram_knows_word_is_same_length():
 
 def test_anagram_returns_anagram_if_anagram():
     assert findAnagram("list", ["istl"]) == ["istl"]
-#
-#
-# def test_anargam_returns_correct_anagrams_when_list_greater_one():
-#     a=Anagram()
-#     assert a.findAnagram("list", ["lit", "it", "otter"]) == []
 
 
-# def test_anargam_returns_correct_anagrams_when_list_greater_one():
-#     a=Anagram()
-#     assert a.findAnagram("listen", ["enlist", "inlets", "google"]) == [
-#         "enlist", "inlets"]
+def test_anargam_returns_correct_anagrams_when_list_greater_one():
+    assert findAnagram("list", ["lit", "it", "otter"]) == []
+
+
+def test_anargam_returns_correct_anagrams_when_list_greater_one():
+    assert findAnagram("listen", ["enlist", "inlets", "google"]) == [
+        "enlist", "inlets"]
