b60e73e45777a5002873d333af88f51b1dc6851e
Angelica <Angelica137@users.noreply.github.com>
2021-07-09 22:35:49 +0100

    remove class, first test pass

--- scripts/anagram.py
+++ scripts/anagram.py
@@ -1,10 +1,9 @@
-class Anagram:
-    def findAnagram(self, str, arr):
-        anagrams = []
-        for item in range(len(arr)):
-            if len(arr[item]) == len(str):
-                sorted_item = sorted(arr[item])
-                sorted_str = sorted(str)
-                if sorted_item == sorted_str:
-                    anagrams.append(arr[item])
-        return anagrams
+def findAnagram(str, arr):
+    anagrams = []
+    for item in range(len(arr)):
+        if len(arr[item]) == len(str):
+            sorted_item = sorted(arr[item])
+            sorted_str = sorted(str)
+            if sorted_item == sorted_str:
+                anagrams.append(arr[item])
+    return anagrams
--- tests/test_anagram.py
+++ tests/test_anagram.py
@@ -1,22 +1,21 @@
-from scripts.anagram import Anagram
+from scripts.anagram import findAnagram
 
 
 def test_findAnagram_knows_word_is_same_length():
-    a = Anagram()
-    assert len(a.findAnagram("list", ["istl"])[0]) == 4
+    assert len(findAnagram("list", ["istl"])[0]) == 4
 
 
-def test_anagram_returns_anagram_if_anagram():
-    a = Anagram()
-    assert a.findAnagram("list", ["istl"]) == ["istl"]
+# def test_anagram_returns_anagram_if_anagram():
+#     a=Anagram()
+#     assert a.findAnagram("list", ["istl"]) == ["istl"]
+#
+#
+# def test_anargam_returns_correct_anagrams_when_list_greater_one():
+#     a=Anagram()
+#     assert a.findAnagram("list", ["lit", "it", "otter"]) == []
 
 
-def test_anargam_returns_correct_anagrams_when_list_greater_one():
-    a = Anagram()
-    assert a.findAnagram("list", ["lit", "it", "otter"]) == []
-
-
-def test_anargam_returns_correct_anagrams_when_list_greater_one():
-    a = Anagram()
-    assert a.findAnagram("listen", ["enlist", "inlets", "google"]) == [
-        "enlist", "inlets"]
+# def test_anargam_returns_correct_anagrams_when_list_greater_one():
+#     a=Anagram()
+#     assert a.findAnagram("listen", ["enlist", "inlets", "google"]) == [
+#         "enlist", "inlets"]
